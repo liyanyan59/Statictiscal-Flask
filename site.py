22e2ef6fa6b8fc609d781c3ed8802fb68c749b16
ly <liyan6987@gmail.com>
2019-01-25 20:24:12 +0800

    LY

--- site.py
+++ site.py
@@ -4,10 +4,19 @@
 # @FileName: site
 # @Software: PyCharm
 # @Official Accounts：大数据学习废话集
+import glob
 import os
 from flask import Flask, render_template, send_from_directory, make_response, send_file
 
 app = Flask(__name__)
+
+MyFolder = '/static/files/'
+pattern = '*.zip'
+
+os.chdir(MyFolder)
+for fname in glob.glob(pattern):
+    if os.path.isfile(fname):
+        key = fname.decode('cp936')
 
 
 @app.route('/')
@@ -15,27 +24,12 @@ def hello_world():
     return render_template('ddd.html')
 
 
-# @app.route("/download/<filepath>", methods=['GET'])
-# def download_file(filepath):
-#     # 此处的filepath是文件的路径，但是文件必须存储在static文件夹下， 比如images\test.jpg
-#     return app.send_static_file(filepath)
-# @app.route("/download/<filename>")
-# def downloader(filename):
-#     dirpath = os.path.join(app.root_path, 'static/files')  # 这里是下在目录，从工程的根目录写起，比如你要下载static/js里面的js文件，这里就要写“static/js”
-#     return send_from_directory(dirpath, filename, as_attachment=True)  # as_attachment=True 一定要写，不然会变成打开，而不是下载
 @app.route("/download/files/<filename>", methods=['GET'])
 def download_file(filename):
-    # 需要知道2个参数, 第1个参数是本地目录的path, 第2个参数是文件名(带扩展名)
-    # directory = os.getcwd()  # 假设在当前目录
-    # dirpath = '/root/flask/static/files'
-    # response = make_response(send_from_directory(app.static_folder, filename, as_attachment=True)) # 把当前数据封装成一个response
-    # response.headers["Content-Disposition"] = "attachment; filename={}".format(filename.encode('utf-8').decode('utf-8')) # 自定义headers
-    # return response
-    path = '/root/flask/static/files'+filename
-    response = make_response(send_file(path))
-    response.headers["Content-Disposition"] = "attachment; filename={};".format(filename)
-    return response
+    fname = filename.encode('cp936')
+    return send_from_directory(MyFolder, fname, mimetype='application/octet-stream')
+
 
 if __name__ == '__main__':
-    # app.run()
-    app.run('0.0.0.0')
+    app.run()
+    # app.run('0.0.0.0')
