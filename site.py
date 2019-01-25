2b69bf9f0ce446fcd45992f3ecfddc582cf26f30
ly <liyan6987@gmail.com>
2019-01-25 19:53:49 +0800

    LY

--- site.py
+++ site.py
@@ -23,12 +23,12 @@ def hello_world():
 # def downloader(filename):
 #     dirpath = os.path.join(app.root_path, 'static/files')  # 这里是下在目录，从工程的根目录写起，比如你要下载static/js里面的js文件，这里就要写“static/js”
 #     return send_from_directory(dirpath, filename, as_attachment=True)  # as_attachment=True 一定要写，不然会变成打开，而不是下载
-@app.route("/download/<filename>", methods=['GET'])
+@app.route("/download/files/<filename>", methods=['GET'])
 def download_file(filename):
     # 需要知道2个参数, 第1个参数是本地目录的path, 第2个参数是文件名(带扩展名)
     # directory = os.getcwd()  # 假设在当前目录
     dirpath = '/root/flask/static/files'
-    response = make_response(send_from_directory(dirpath, filename, as_attachment=True)) # 把当前数据封装成一个response
+    response = make_response(send_from_directory(app.static_folder, filename, as_attachment=True)) # 把当前数据封装成一个response
     response.headers["Content-Disposition"] = "attachment; filename={}".format(filename.encode('utf-8').decode('utf-8')) # 自定义headers
     return response
 
