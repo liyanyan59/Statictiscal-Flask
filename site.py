64791eb80330af028f4bd7eae5f1235dc15620f9
ly <liyan6987@gmail.com>
2019-01-25 19:07:25 +0800

    LY

--- site.py
+++ site.py
@@ -28,8 +28,8 @@ def download_file(filename):
     # 需要知道2个参数, 第1个参数是本地目录的path, 第2个参数是文件名(带扩展名)
     directory = os.getcwd()  # 假设在当前目录
     dirpath = os.path.join(directory, 'static/files')
-    response = make_response(send_from_directory(dirpath, filename, as_attachment=True))
-    response.headers["Content-Disposition"] = "attachment; filename={}".format(filename.encode().decode('latin-1'))
+    response = make_response(send_from_directory(dirpath, filename, as_attachment=True)) # 把当前数据封装成一个response
+    response.headers["Content-Disposition"] = "attachment; filename={}".format(filename.encode('utf-8').decode('utf-8')) # 自定义headers
     return response
 
 if __name__ == '__main__':
