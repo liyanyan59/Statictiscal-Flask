e4369b7de06224177b6749bc9549cb1ee4cd6cec
LY <981770608@qq.com>
2019-05-28 21:14:36 +0800

    LY的烂代码

--- utils.py
+++ utils.py
@@ -11,12 +11,12 @@ from selenium import webdriver
 
 
 def get_url():
-    url = 'https://login.aliexpress.com/buyer.htm'
+    url = 'https://www.aliexpress.com'
     # 配置option
     option = webdriver.ChromeOptions()
-    option.add_argument("--headless")
+    # option.add_argument("--headless")
     option.add_argument('--no-sandbox')
-    option.add_argument('--disable-gpu')
+    # option.add_argument('--disable-gpu')
     option.add_argument('--log-level=3')
     # 不显示图片
     prefs = {"profile.managed_default_content_settings.images": 2}
@@ -25,6 +25,9 @@ def get_url():
     driver = webdriver.Chrome(chrome_options=option)
     driver.implicitly_wait(4)  # 最长加载时间
     driver.get(url)
+
+    driver.find_element_by_xpath('//input[@name="SearchText"]').send_keys("iphone7")
+    driver.find_element_by_xpath('//input[@class="search-button"]').submit()
 
     # 阿里反爬,跳转登录的处理
     if 'login' in driver.current_url:
@@ -35,25 +38,23 @@ def get_url():
         driver.find_element_by_xpath('//button[@class="fm-button fm-submit password-login"]').click()
         time.sleep(3)
 
-    # driver.find_element_by_xpath('//input[@name="SearchText"]').send_keys("iphone7")
-    # driver.find_element_by_xpath('//input[@class="search-button"]').submit()
-    url = 'https://www.aliexpress.com/wholesale?SearchText=iphone5'
+    # url = 'https://www.aliexpress.com/wholesale?SearchText=iphone5'
 
-    driver.get(url)
+    # driver.get(url)
 
     infos = {}
     index = 1
-    _max = 480
+    _max = 30
     while len(infos) < _max:
         # time.sleep(3)
 
-        # 弹窗处理(登录后无弹窗)
-        # if '<span class="ui-pagination-active">1</span>' in driver.page_source:
-        #     try:
-        #         element = driver.find_element_by_xpath('//a[@class="close-layer"]')
-        #         driver.executeScript("arguments[0].click();", element)
-        #     except selenium.common.exceptions.NoSuchElementException:
-        #         pass
+        # 弹窗处理
+        if '<span class="ui-pagination-active">1</span>' in driver.page_source:
+            try:
+                element = driver.find_element_by_xpath('//a[@class="close-layer"]')
+                driver.execute_script("arguments[0].click();", element)
+            except selenium.common.exceptions.NoSuchElementException:
+                pass
 
         temp_list = driver.find_elements_by_xpath('//a[@class="item-title"]')
 
@@ -67,6 +68,8 @@ def get_url():
         driver.find_element_by_xpath('//a[@class="page-next ui-pagination-next"]').click()
 
     infos = {k: infos[k] for k in list(infos.keys())[:_max]}
+
+    driver.quit()
 
     return infos
 
